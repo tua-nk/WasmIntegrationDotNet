6fe520a57733206e50c4312708256cee30d7d7c4
thaont-ulis <102590564+tua-nk@users.noreply.github.com>
2023-05-07 21:41:43 +0700

    Integrate the "Rotating Triangle" into the "HTML5 Core"

--- WebApplication1/Html5Core/Src/BuildC.bat
+++ WebApplication1/Html5Core/Src/BuildC.bat
@@ -1,1 +1,3 @@
-emcc html5_core.c -o html5_core.html -s USE_WEBGL2=1 -s WASM=1 -s FULL_ES3=1 -s NO_EXIT_RUNTIME=1 -std=c17 -s ALLOW_MEMORY_GROWTH=1
+rem emcc html5_core.c -o html5_core.html -s USE_WEBGL2=1 -s WASM=1 -s FULL_ES3=1 -s NO_EXIT_RUNTIME=1 -std=c17 -s ALLOW_MEMORY_GROWTH=1
+
+emcc html5_core.cpp -o html5_core.html -s USE_WEBGL2=1 -s WASM=1 -s FULL_ES3=1 -s NO_EXIT_RUNTIME=1 -std=c++17 -s ALLOW_MEMORY_GROWTH=1 -s FORCE_FILESYSTEM
